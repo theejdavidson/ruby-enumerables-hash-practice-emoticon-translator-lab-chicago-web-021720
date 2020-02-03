e7b1d05757d96437da9d3350b7a792567f770b8b
Ethan Davidson <theejdavidson@gmail.com>
2020-02-03 22:52:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,11 @@ def load_library(lib)
   # code goes here
   library = YAML.load_file(lib)
   
-  pp library
+  #pp library
+  # the keys inside get_meaning hash should be japanese emoticons
+  get_meaning = library.reduce({}) { |memo, (meaning, emoji_arr)| 
+  memo.store(emoji_arr[1], meaning) }
+  pp get_meaning
   hash = {"get_meaning":  {}, "get_emoticon": {}}
   return hash
 end
