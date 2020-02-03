7aebd4fd34403b427e45128f9f4e560296b09847
Ethan Davidson <theejdavidson@gmail.com>
2020-02-03 22:29:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,11 @@
 # require modules here
+require "yaml"
 
-def load_library
+def load_library(lib)
   # code goes here
+  library = YAML.load_file(lib)
+  hash = {:get_meaning => {}, :get_emoticon => {}}
+  return library
 end
 
 def get_japanese_emoticon
